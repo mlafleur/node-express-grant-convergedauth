bf452a6c38bfe406d95fae6dfefe952ccfa258f2
Marc LaFleur <marc@massivescale.com>
2016-04-14 11:24:16 -0400

    Updated to Grant 3.6.0
    Grant v3.6.0 includes v2 Endpoint support out of the box now. Updated to
    the new version and removed some code no longer needed.

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 // Reference express, express-session and grant packages
-var express = require('express'), session = require('express-session'), Grant = require('grant-express') 
+var express = require('express'), session = require('express-session'), Grant = require('grant-express')
 
 // Create an instance of Grant and configure it for the micrsoft v2 auth endpoint
 var grant = new Grant({
-        "server": {
-            "protocol": "http",
-            "host": "localhost:3000",
-            "transport": "session"
-        },
-        "microsoft": {
-            "authorize_url": "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
-            "access_url": "https://login.microsoftonline.com/common/oauth2/v2.0/token",
-            "oauth": 2,
-            "scope_delimiter": " ",
-            "key": "fc9a6fcf-bcd0-451c-9641-12ca455b9a34",
-            "secret": "tXVebOX8jdhgv5i6HN9Ep9w",
-            "scope":  ["https://graph.microsoft.com/User.Read", "offline_access", "openid"],
-            "callback": "/microsoft/callback"
-        }
-    });
+    "server": {
+        "protocol": "http",
+        "host": "localhost:3000",
+        "transport": "session"
+    },
+    "microsoft": {
+        "key": "fc9a6fcf-bcd0-451c-9641-12ca455b9a34",
+        "secret": "tXVebOX8jdhgv5i6HN9Ep9w",
+        "scope": ["https://graph.microsoft.com/User.Read", "offline_access", "openid"],
+        "callback": "/loggedIn"
+    }
+});
 
 // Create an instance of express using our session and grant instances 
 var app = express();
@@ -31,9 +27,10 @@ app.get('/', function(req, res) {
 });
 
 // This is the callback we land at after authenication
-app.get('/microsoft/callback', function(req, res) {    
-    res.end(JSON.stringify(req.session.grant.response.raw, null, 2))
-})
+app.get('/loggedIn', function(req, res) {
+    var result = '<pre>' + JSON.stringify(req.session.grant.response.raw, null, 2) + '</pre>';
+    res.end(result)
+});
 
 // Start listening on port 3000
 app.listen(3000, function() {
